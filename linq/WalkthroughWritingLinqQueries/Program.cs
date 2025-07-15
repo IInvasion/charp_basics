348bdb459ef3e730c515da129e1790990d267f00
Evgeny Invasion <invasion@deeplay.io>
2025-07-15 11:29:05 +0700

    Add linq

---
+++ linq/WalkthroughWritingLinqQueries/Program.cs
@@ -0,0 +1,150 @@
+﻿using WalkthroughWritingLinqQueries;
+
+// Create a data source by using a collection initializer.
+IEnumerable<Student> students =
+[
+    new Student(First: "Svetlana", Last: "Omelchenko", ID: 111, Scores: [97, 92, 81, 60]),
+    new Student(First: "Claire",   Last: "O'Donnell",  ID: 112, Scores: [75, 84, 91, 39]),
+    new Student(First: "Sven",     Last: "Mortensen",  ID: 113, Scores: [88, 94, 65, 91]),
+    new Student(First: "Cesar",    Last: "Garcia",     ID: 114, Scores: [97, 89, 85, 82]),
+    new Student(First: "Debra",    Last: "Garcia",     ID: 115, Scores: [35, 72, 91, 70]),
+    new Student(First: "Fadi",     Last: "Fakhouri",   ID: 116, Scores: [99, 86, 90, 94]),
+    new Student(First: "Hanying",  Last: "Feng",       ID: 117, Scores: [93, 92, 80, 87]),
+    new Student(First: "Hugo",     Last: "Garcia",     ID: 118, Scores: [92, 90, 83, 78]),
+
+    new Student("Lance",   "Tucker",      119, [68, 79, 88, 92]),
+    new Student("Terry",   "Adams",       120, [99, 82, 81, 79]),
+    new Student("Eugene",  "Zabokritski", 121, [96, 85, 91, 60]),
+    new Student("Michael", "Tucker",      122, [94, 92, 91, 91])
+];
+
+// Create the query.
+// The first line could also be written as "var studentQuery ="
+IEnumerable<Student> studentQuery =
+    from student in students
+    where student.Scores[0] > 90
+    select student;
+
+// Execute the query.
+// var could be used here also.
+foreach (Student student in studentQuery)
+{
+    Console.WriteLine($"{student.Last}, {student.First}");
+}
+
+Console.WriteLine("-----------------------------------");
+
+IEnumerable<Student> studentQuery2 =
+    from student in students
+    where student.Scores[0] > 90 && student.Scores[3] < 80
+    // orderby student.Last ascending
+    orderby student.Scores[0] descending
+    select student;
+
+foreach (Student student in studentQuery2)
+{
+    Console.WriteLine($"{student.Last}, {student.First} {student.Scores[0]}");
+}
+
+Console.WriteLine("-----------------------------------");
+
+IEnumerable<IGrouping<char, Student>> studentQuery3 =
+    from student in students
+    group student by student.Last[0];
+
+foreach (IGrouping<char, Student> studentGroup in studentQuery3)
+{
+    Console.WriteLine(studentGroup.Key);
+    foreach (Student student in studentGroup)
+    {
+        Console.WriteLine($"   {student.Last}, {student.First}");
+    }
+}
+
+Console.WriteLine("-----------------------------------");
+
+IEnumerable<IGrouping<char, Student>> studentQuery4 =
+    from student in students
+    group student by student.Last[0];
+
+foreach (IGrouping<char, Student> studentGroup in studentQuery4)
+{
+    Console.WriteLine(studentGroup.Key);
+    foreach (Student student in studentGroup)
+    {
+        Console.WriteLine($"   {student.Last}, {student.First}");
+    }
+}
+
+Console.WriteLine("-----------------------------------");
+
+var studentQuery5 =
+    from student in students
+    group student by student.Last[0] into studentGroup
+    orderby studentGroup.Key
+    select studentGroup;
+
+foreach (var groupOfStudents in studentQuery5)
+{
+    Console.WriteLine(groupOfStudents.Key);
+    foreach (var student in groupOfStudents)
+    {
+        Console.WriteLine($"   {student.Last}, {student.First}");
+    }
+}
+
+Console.WriteLine("-----------------------------------");
+
+// This query returns those students whose
+// first test score was higher than their
+// average score.
+var studentQuery6 =
+    from student in students
+    let totalScore = student.Scores[0] + student.Scores[1] +
+        student.Scores[2] + student.Scores[3]
+    where totalScore / 4 < student.Scores[0]
+    select $"{student.Last}, {student.First}";
+
+foreach (string s in studentQuery6)
+{
+    Console.WriteLine(s);
+}
+
+
+Console.WriteLine("-----------------------------------");
+
+var studentQuery7 =
+    from student in students
+    let totalScore = student.Scores[0] + student.Scores[1] +
+        student.Scores[2] + student.Scores[3]
+    select totalScore;
+
+double averageScore = studentQuery7.Average();
+Console.WriteLine($"Class average score = {averageScore}");
+
+Console.WriteLine("-----------------------------------");
+
+IEnumerable<string> studentQuery8 =
+    from student in students
+    where student.Last == "Garcia"
+    select student.First;
+
+Console.WriteLine("The Garcias in the class are:");
+foreach (string s in studentQuery8)
+{
+    Console.WriteLine(s);
+}
+
+Console.WriteLine("-----------------------------------");
+
+var aboveAverageQuery8 =
+    from student in students
+    let x = student.Scores[0] + student.Scores[1] +
+        student.Scores[2] + student.Scores[3]
+    where x > averageScore
+    select new { id = student.ID, score = x };
+
+foreach (var item in aboveAverageQuery8)
+{
+    Console.WriteLine("Student ID: {0}, Score: {1}", item.id, item.score);
+}
